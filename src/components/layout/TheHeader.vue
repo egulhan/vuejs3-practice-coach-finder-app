a4cb251fd6e6e27590f39660b468eb0fe39be706
Erman Gülhan <mr.ermangulhan@gmail.com>
2022-06-20 17:28:03 +0300

    make all coaches link active as well while visiting at home page

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,9 +1,11 @@
 <template>
   <header>
-    <h2><router-link class="home" :to="{name: 'home'}">Find a coach</router-link></h2>
+    <h2>
+      <router-link class="home" :to="{name: 'home'}">Find a coach</router-link>
+    </h2>
     <ul>
       <li>
-        <router-link class="active" :to="{name: 'coaches'}">All Coaches</router-link>
+        <router-link :class="{'router-link-active': $route.name==='home'}" :to="{name: 'coaches'}">All Coaches</router-link>
       </li>
       <li>
         <router-link :to="{name: 'requests'}">Requests</router-link>
