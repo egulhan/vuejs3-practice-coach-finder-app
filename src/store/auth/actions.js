089e23df94f084c0cf3eeaa6bff2704718304751
Erman Gülhan <mr.ermangulhan@gmail.com>
2022-06-23 16:45:21 +0300

    implement auto logout functionality based on expiration time provided on firebase

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -1,3 +1,14 @@
+import router from "@/router";
+
+let autoLogoutTimer;
+
+const handleAuthExpiration = (context, expirationTimeInMs) => {
+    autoLogoutTimer = setTimeout(() => {
+        context.commit('logout');
+        router.replace({name: 'coaches'});
+    }, expirationTimeInMs);
+};
+
 export default {
     setAuthUser(context, payload) {
         context.commit('setAuthUser', payload);
@@ -26,12 +37,14 @@ export default {
 
         const jsonResponse = await response.json();
         const {localId: userId, email, idToken: token, expiresIn} = jsonResponse;
+        const expirationTimeInMs = +expiresIn * 1000;
+        const expirationDate = new Date().getTime() + expirationTimeInMs;
 
         const authUserData = {
             userId,
             email,
             token,
-            expiresIn,
+            expirationDate,
         };
 
         context.commit('setAuthUser', authUserData);
@@ -39,7 +52,10 @@ export default {
         localStorage.setItem('userId', userId);
         localStorage.setItem('email', email);
         localStorage.setItem('token', token);
-        localStorage.setItem('expiresIn', expiresIn);
+        localStorage.setItem('expirationTimeInMs', expirationTimeInMs.toString());
+        localStorage.setItem('expirationDate', expirationDate.toString());
+
+        handleAuthExpiration(context, expirationTimeInMs);
 
         return jsonResponse;
     },
@@ -49,24 +65,36 @@ export default {
         localStorage.removeItem('userId');
         localStorage.removeItem('email');
         localStorage.removeItem('token');
-        localStorage.removeItem('expiresIn');
+        localStorage.removeItem('expirationTimeInMs');
+        localStorage.removeItem('expirationDate');
+
+        clearTimeout(autoLogoutTimer);
     },
     tryAutoLogin(context) {
         const userId = localStorage.getItem('userId');
-        const email = localStorage.getItem('email');
-        const token = localStorage.getItem('token');
-        const expiresIn = localStorage.getItem('expiresIn');
 
         if (!userId) {
             return;
+        }
+
+        const email = localStorage.getItem('email');
+        const token = localStorage.getItem('token');
+        const expirationTimeInMs = localStorage.getItem('expirationTimeInMs');
+        const expirationDate = localStorage.getItem('expirationDate');
+
+        if (new Date().getTime() - expirationDate > 0) {
+            context.commit('logout');
+            return router.replace({name: 'coaches'});
         }
 
         const authUserData = {
             userId,
             email,
             token,
-            expiresIn,
+            expirationDate,
         };
+
+        handleAuthExpiration(context, expirationTimeInMs);
 
         context.commit('setAuthUser', authUserData);
     }
